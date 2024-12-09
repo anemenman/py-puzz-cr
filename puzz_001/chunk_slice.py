ff9a5352f374142f125b5e8de66e00073f035a72
aa <aa@MacBook-Pro-aa.local>
2024-12-09 18:26:20 +0200

    20241209 slice puzz

---
+++ puzz_001/chunk_slice.py
@@ -0,0 +1,76 @@
+text = 'Python 1234567890'
+
+sliced_text = slice(6)
+print(text[sliced_text])
+
+numbers1 = [i for i in range(2, 99, 2)]
+print(numbers1)
+
+numbers2 = list(range(2, 99, 2))
+numbers2.append(99)
+print(numbers2)
+
+
+def chunk(in_list, size):
+    if size <= 0:
+        raise ValueError(f'in_list must be greater than zero {size}')
+    in_list_copy = in_list.copy()
+    result = []
+    last_chunk = []
+    while in_list_copy:
+        if len(last_chunk) == size:
+            result.append(last_chunk)
+            last_chunk = []
+        last_chunk.append(in_list_copy[0])
+        del in_list_copy[0]
+
+    return result
+
+
+def chunk_slice(in_list, size):
+    print("--->")
+    print(list(range(0, len(in_list), size)))
+    return [in_list[i:i + size] for i in range(0, len(in_list), size)]
+
+
+chunk_res = chunk(numbers2, 3)
+print(chunk_res)
+
+chunk_res2 = chunk_slice(numbers2, 3)
+print(chunk_res2)
+
+
+def check_chunk_negative():
+    try:
+        res = chunk(numbers2, -1)
+    except ValueError:
+        pass
+    else:
+        assert False
+
+
+check_chunk_negative()
+
+
+def sq_first_two_chunk(chunks):
+    i = 2
+    if len(chunks) < 2:
+        i = len(chunks)
+    for j in range(i):
+        chunks[j] = [k ** 2 for k in chunks[j]]
+
+    return chunks
+
+
+def merge_chunks(chunks):
+    res = []
+    for chunk1 in chunks:
+        res.extend(chunk1)
+
+    return res
+
+
+chunk_res3 = sq_first_two_chunk(chunk_res2)
+print(chunk_res3)
+res3 = merge_chunks(chunk_res3)
+print(res3)
