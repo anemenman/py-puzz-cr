32aef91f1392b825a0fac7c832b5b8f0bdcfeb3a
aa <aa@MacBook-Pro-aa.local>
2025-04-21 22:52:25 +0300

    20250421 tuple unchangeable

---
+++ puzz_001_commons/pz027_tuple_unchangeable.py
@@ -0,0 +1,6 @@
+# tuple
+data = ('a', 'b', 'c')
+print(data)
+
+# AttributeError: 'tuple' object has no attribute 'add'
+# data.add('e')#Tuples are unchangeable, meaning that you cannot change, add, or remove items once the tuple is created.
