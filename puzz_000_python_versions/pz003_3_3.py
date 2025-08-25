955361d288bf309e542ca70646965b3473d486ad
aa <aa@MacBook-Pro-aa.local>
2025-08-25 14:01:13 +0300

    20250825 3.2 v

---
+++ puzz_000_python_versions/pz003_3_3.py
@@ -0,0 +1,35 @@
+"""
+Python 3.3.0, documentation released on 29 September 2012.
+
+New syntax features:
+
+New yield from expression for generator delegation.
+The u'unicode' syntax is accepted again for str objects.
+New library modules:
+
+faulthandler (helps debugging low-level crashes)
+ipaddress (high-level objects representing IP addresses and masks)
+lzma (compress data using the XZ / LZMA algorithm)
+venv (Python virtual environments, as in the popular virtualenv package)
+New built-in features:
+
+Reworked I/O exception hierarchy.
+Implementation improvements:
+
+Rewritten import machinery based on importlib.
+More compact unicode strings.
+More compact attribute dictionaries.
+Security improvements:
+
+Hash randomization is switched on by default.
+"""
+print("Python 3.3.0")
+
+"""PEP 405 - Python Virtual Environments
+
+This PEP adds the venv module for programmatic access, and the pyvenv script for command-line access and administration.
+"""
+
+"""PEP 420: Namespace Packages
+
+Native support for package directories that don’t require __init__.py marker files and can automatically span multiple path segments"""
