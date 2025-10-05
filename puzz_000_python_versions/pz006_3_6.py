59f67afa07d9a872de1d41c849e0e07a4093dc00
aa <aa@MacBook-Pro-aa.local>
2025-10-05 17:00:44 +0300

    20251005 py 3.6 version secrets

--- puzz_000_python_versions/pz006_3_6.py
+++ puzz_000_python_versions/pz006_3_6.py
@@ -14,6 +14,9 @@ New library modules:
 secrets: PEP 506 – Adding A Secrets Module To The Standard Library.
 """
 import asyncio
+from os import urandom
+from random import randrange, shuffle, sample
+from secrets import choice
 from typing import Dict
 
 """PEP 498: Formatted string literals"""
@@ -45,3 +48,20 @@ async def ticker(delay, to):
 # result = [i async for i in aiter() if i % 2]
 # print(result)
 # result = [await fun() for fun in funcs if await condition()]
+
+"""
+New Modules
+secrets
+The main purpose of the new secrets module is to provide an obvious way to reliably generate cryptographically strong 
+pseudo-random values suitable for managing secrets, such as account authentication, tokens, and similar.
+"""
+print(urandom(10))
+print(randrange(10))  # Integer from 0 to 9 inclusive
+print(choice(['win', 'lose', 'draw']))  # Single random element from a sequence
+
+deck = 'ace two three four'.split()
+shuffle(deck)  # Shuffle a list
+print(deck)
+
+shuffled1 = sample([10, 20, 30, 40, 50], k=4)
+print(shuffled1)  # Four samples without replacement
